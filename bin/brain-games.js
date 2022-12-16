2ab187b6c453593367cba84e56f3459171dcdb9c
Yury Lavrukhin <me@yurylavrukhin.com>
2022-12-16 23:28:47 +0700

    feat: setup eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import { askName } from './cli.js';
 
-import readlineSync from 'readline-sync';
-
-const name = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${name}!`)
+askName();
